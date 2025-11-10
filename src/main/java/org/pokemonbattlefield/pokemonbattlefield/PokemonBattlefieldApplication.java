2af265ca55b23774d6c1f5766555a11b799defe3
João Lucas <jlso9866@gmail.com>
2025-11-10 20:08:35 -0300

    first commit, just uploading the schemas

---
+++ src/main/java/org/pokemonbattlefield/pokemonbattlefield/PokemonBattlefieldApplication.java
@@ -0,0 +1,13 @@
+package org.pokemonbattlefield.pokemonbattlefield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBattlefieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBattlefieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pokemonbattlefield/pokemonbattlefield/PokemonBattlefieldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pokemonbattlefield.pokemonbattlefield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBattlefieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
